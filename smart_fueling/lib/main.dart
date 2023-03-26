2c0a68526059f8ece7793ca072af7eaf36999c10
Karthik.R <karthikr2712@gmail.com>
2023-03-26 17:45:00 +0530

    Userlocation

--- smart_fueling/lib/main.dart
+++ smart_fueling/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart' as loc;
@@ -25,10 +27,52 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final Completer<GoogleMapController> controllerMap =
+      Completer<GoogleMapController>();
+
+  CameraPosition _userCamera = CameraPosition(target: LatLng(13.0827, 80.2707));
+
   loc.Location location = loc.Location();
+
+  LatLng currentLocation = LatLng(13.0827, 80.2707);
+
+  Future<void> _getUserLocation({required loc.Location location}) async {
+    bool _serviceEnabled;
+    loc.PermissionStatus _permissionGranted;
+    loc.LocationData _locationData;
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == loc.PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != loc.PermissionStatus.granted) {
+        return;
+      }
+    }
+    _locationData = await location.getLocation();
+    print('Latitude & Lng of device :$_locationData.latitude');
+
+    final GoogleMapController controller = await controllerMap.future;
+    setState(() {
+      currentLocation =
+          LatLng(_locationData.latitude!, _locationData.longitude!);
+      print('Latitude cur:${currentLocation.latitude}');
+      print('Longitude cur:${currentLocation.longitude}');
+
+      controller.animateCamera(CameraUpdate.newCameraPosition(
+          _userCamera = CameraPosition(target: currentLocation)));
+    });
+  }
 
   @override
   void initState() {
+    _getUserLocation(location: location);
     super.initState();
   }
 
@@ -45,13 +89,17 @@ class _HomePageState extends State<HomePage> {
         ),
         body: Stack(children: [
           GoogleMap(
-              initialCameraPosition: CameraPosition(
-            target: LatLng(
-              13.0827,
-              80.2707,
+            initialCameraPosition: CameraPosition(
+              target: currentLocation,
+              zoom: 16,
             ),
-            zoom: 16,
-          )),
+            markers: {
+              Marker(
+                markerId: MarkerId('currentLocation'),
+                position: currentLocation,
+              )
+            },
+          ),
           Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.center,
@@ -64,25 +112,5 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-Future<dynamic> getLocation({required loc.Location location}) async {
-  bool _serviceEnabled;
-  loc.PermissionStatus _permissionGranted;
-  loc.LocationData _locationData;
-  _serviceEnabled = await location.serviceEnabled();
-  if (!_serviceEnabled) {
-    _serviceEnabled = await location.requestService();
-    if (!_serviceEnabled) {
-      return;
-    }
-  }
+//Attaining Device location
 
-  _permissionGranted = await location.hasPermission();
-  if (_permissionGranted == loc.PermissionStatus.denied) {
-    _permissionGranted = await location.requestPermission();
-    if (_permissionGranted != loc.PermissionStatus.granted) {
-      return;
-    }
-  }
-  _locationData = await location.getLocation();
-  return _locationData;
-}
