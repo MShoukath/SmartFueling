3d694ed1f20751e046972b0e82d157a9563cd4fc
Karthik.R <karthikr2712@gmail.com>
2023-05-08 15:14:56 +0530

    user profile

--- smart_fueling/lib/screens/user_profile.dart
+++ smart_fueling/lib/screens/user_profile.dart
@@ -16,15 +16,26 @@ class _UserProfileState extends State<UserProfile> {
   var name = " ";
   var email = " ";
   var userid = " ";
+  var mileage = "";
+  var tankCapacity = "";
+
   @override
   Widget build(BuildContext context) {
     final user = FirebaseAuth.instance.currentUser;
+    Stream users = FirebaseFirestore.instance.collection('Users').snapshots();
+
+    // users.listen((event) {
+    //   var docs = event.snapshot.data!.docs;
+    //   mileage = docs[0]['mileage'];
+    //   print(mileage);
+    // });
 
     if (user != null) {
-      name = user.displayName.toString();
+      user.updateDisplayName("Karthik");
       email = user.email.toString();
       userid = user.uid;
     }
+
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -36,48 +47,80 @@ class _UserProfileState extends State<UserProfile> {
         title: const Text("Profile"),
       ),
       body: Container(
+        height: 400,
         padding: const EdgeInsets.all(20),
-        child: ListView(
-          children: [
-            ListTile(
-              leading: Icon(Icons.account_circle_outlined),
-              title: Text('User Name : $name'),
-            ),
-            ListTile(
-              leading: Icon(Icons.email_outlined),
-              title: Text('EmailID : $email'),
-            ),
-            ListTile(
-              leading: Icon(Icons.gas_meter_outlined),
-              title: Text('Mileage : '),
-            ),
-            ListTile(
-              leading: Icon(Icons.local_gas_station_outlined),
-              title: Text('Tank Capacity :'),
-            ),
-            SizedBox(height: 10),
-            ElevatedButton.icon(
-                onPressed: () {
-                  FirebaseAuth.instance.signOut();
-                  Navigator.pushReplacement(context,
-                      MaterialPageRoute(builder: (context) => SignInScreen()));
-                },
-                icon: Icon(Icons.logout_outlined),
-                label: Text('Logout')),
-            SizedBox(
-              height: 10,
-            ),
-            Card(
-                elevation: 6,
-                child: Column(children: [
-                  Text(
-                    'History of Trips',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  Trip()
-                ])),
-          ],
-        ),
+        child: StreamBuilder(
+            stream: users,
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return Text('Connection error');
+              } else if (snapshot.connectionState == ConnectionState.waiting) {
+                return Text('Loading......');
+              } else {
+                var docs = snapshot.data!.docs;
+                mileage = docs[0]['mileage'];
+                tankCapacity = docs[0]['tank_capacity'];
+                return ListView(
+                  children: [
+                    ListTile(
+                      leading: Icon(Icons.account_circle_outlined),
+                      title: Text('User Name : $name'),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.email_outlined),
+                      title: Text('EmailID : $email'),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.gas_meter_outlined),
+                      title: Text('Mileage : $mileage'),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.local_gas_station_outlined),
+                      title: Text('Tank Capacity : $tankCapacity'),
+                    ),
+                    SizedBox(height: 10),
+                    ElevatedButton.icon(
+                        onPressed: () {
+                          FirebaseAuth.instance.signOut();
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SignInScreen()));
+                        },
+                        icon: Icon(Icons.logout_outlined),
+                        label: Text('Logout')),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Card(
+                        elevation: 6,
+                        child: Column(children: [
+                          Text(
+                            'History of Trips',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          Trip()
+                        ])),
+                  ],
+                );
+                // return ListView.builder(
+                //     itemCount: 1,
+                //     itemBuilder: (context, index) {
+                //       return ListTile(
+                //           title: SizedBox(
+                //         child: Column(
+                //           children: [
+                //             Text("Username : $name"),
+                //             Text("Email ID : $email"),
+                //             Text("Tank Capacity : " +
+                //                 docs[index]['tank_capacity']),
+                //             Text(" Mileage : " + docs[index]['mileage']),
+                //           ],
+                //         ),
+                //       ));
+                //     });
+              }
+            }),
       ),
     );
   }
